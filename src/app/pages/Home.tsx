62ce26c8d52a230b34d14efac5a004c383338f12
ReetikaNEU <bhanushali.re@northeastern.edu>
2024-12-26 22:17:40 -0500

    modified home page - better ui

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -8,9 +8,14 @@ import {
   Typography,
 } from "@mui/material";
 import React, { useEffect } from "react";
-import logo from "../../static/favicon.png";
+import logo from "../../static/favicon.jpg";
+import cmu from "../../static/neu.jpg"
 import { useLocation } from "react-router-dom";
 import { links } from "./links";
+import './Home.css'
+import Divider from "@mui/material/Divider";
+import MDContainer from "../components/MDContainer";
+import GitHubCalendar, {createCalendarTheme} from "react-github-calendar";
 
 interface Props {
   setSelectedIndex: React.Dispatch<React.SetStateAction<number>>;
@@ -27,56 +32,94 @@ export default function Home({ setSelectedIndex }: Props) {
   }, [pathname]);
 
   return (
-    <Grid
-      container
-      spacing={0}
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      sx={{ minHeight: `calc(100vh - 20px - 33px)` }}
-    >
-      <Grid item xs={3}>
-        <Stack direction={{ xs: "column", sm: "row-reverse" }} spacing={2}>
-          <Box display="flex" sx={{ justifyContent: "center" }}>
-            <img src={logo} width="100px" alt="logo" />
-          </Box>
-          <Box>
-            <Grid
-              display="flex"
-              justifyContent={{ xs: "center", sm: "flex-start" }}
-            >
-              <Typography variant="h3">{process.env.REACT_APP_NAME}</Typography>
-            </Grid>
-            <Grid
-              display="flex"
-              justifyContent={{ xs: "center", sm: "flex-start" }}
-            >
-              <Typography variant="subtitle1" gutterBottom>
-                Always difficult, always beautiful
-              </Typography>
-            </Grid>
-            <Grid
-              display="flex"
-              justifyContent={{ xs: "center", sm: "flex-start" }}
-            >
-              <Stack direction="row" spacing={0.4}>
-                {links.map((link) => (
-                  <Tooltip key={link.index} title={link.title} arrow>
-                    <Link
+      <>
+        <div id="main-container">
+          <img src={cmu} alt={"Header"} id="cmu-header"/>
+          <img src={logo} id="my-logo" alt={"Logo"}/>
+        </div>
+        <div id="title-container">
+          <Typography variant="h3">Reetika Bhanushali</Typography>
+          <Typography variant="h6">Graduate Student @ Northeastern University</Typography>
+          <Stack direction="row" spacing={0.4}>
+            {links.map((link) => (
+                <Tooltip key={link.index} title={link.title} arrow>
+                  <Link
                       target="_blank"
                       href={link.href}
                       underline="none"
                       color="inherit"
-                    >
-                      <IconButton color="inherit">{link.icon}</IconButton>
-                    </Link>
-                  </Tooltip>
-                ))}
-              </Stack>
-            </Grid>
-          </Box>
-        </Stack>
-      </Grid>
-    </Grid>
+                  >
+                    <IconButton color="inherit">{link.icon}</IconButton>
+                  </Link>
+                </Tooltip>
+            ))}
+          </Stack>
+            <Divider style={{marginTop: "20px"}}/>
+            <Typography style={{marginTop: "20px", marginBottom: "20px"}} variant="h6">As an Information Systems professional with nearly three years of experience, I specialize in Java development, Spring frameworks, and cloud solutions, particularly Amazon Web Services. Throughout my career, I have driven significant technological advancements in roles ranging from a Consultant at JP Morgan Chase & Co. to a Software Engineering Analyst at Accenture. My projects have ranged from optimizing SQL queries for improved performance to pioneering a Sign Language Interpreter to aid communication for the hearing impaired. Currently pursuing a Master’s degree at Northeastern University, I am dedicated to continuous personal and professional growth. I excel in environments that challenge my problem-solving abilities and offer opportunities for innovation. Whether developing complex software solutions or leading data-driven projects, I am committed to leveraging technology to create impactful solutions.</Typography>
+            <GitHubCalendar username="ReetikaNEU" theme={createCalendarTheme("green", "#2D333B")}/>
+        </div>
+      </>
   );
+
+  // return (
+  //   <Grid
+  //     container
+  //     spacing={0}
+  //     direction="column"
+  //     alignItems="center"
+  //     justifyContent="center"
+  //     sx={{ minHeight: `calc(100vh - 20px - 33px)` }}
+  //   >
+  //     <Grid item xs={3}>
+  //       <Stack direction={{ xs: "column", sm: "row-reverse" }} spacing={2}>
+  //         <Box display="flex" sx={{ justifyContent: "center" }}>
+  //           <img src={logo} width="140px" height="140px" alt="logo" />
+  //         </Box>
+  //         <Box>
+  //           <Grid
+  //             display="flex"
+  //             justifyContent={{ xs: "center", sm: "flex-start" }}
+  //           >
+  //             <Typography variant="h3">Femin Dharamshi</Typography>
+  //           </Grid>
+  //           <Grid
+  //             display="flex"
+  //             justifyContent={{ xs: "center", sm: "flex-start" }}
+  //           >
+  //             <Typography variant="subtitle1" gutterBottom>
+  //               Always difficult, always beautiful
+  //               {/* Better an{' '}
+  //               <Box fontWeight="fontWeightMedium" display="inline">
+  //                 oops
+  //               </Box>{' '}
+  //               than a{' '}
+  //               <Box fontWeight="fontWeightMedium" display="inline">
+  //                 what if
+  //               </Box> */}
+  //             </Typography>
+  //           </Grid>
+  //           <Grid
+  //             display="flex"
+  //             justifyContent={{ xs: "center", sm: "flex-start" }}
+  //           >
+  //             <Stack direction="row" spacing={0.4}>
+  //               {links.map((link) => (
+  //                 <Tooltip key={link.index} title={link.title} arrow>
+  //                   <Link
+  //                     target="_blank"
+  //                     href={link.href}
+  //                     underline="none"
+  //                     color="inherit"
+  //                   >
+  //                     <IconButton color="inherit">{link.icon}</IconButton>
+  //                   </Link>
+  //                 </Tooltip>
+  //               ))}
+  //             </Stack>
+  //           </Grid>
+  //         </Box>
+  //       </Stack>
+  //     </Grid>
+  //   </Grid>
+  // );
 }
